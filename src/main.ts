0d103ecde092fe3a97d4568b6269392ff6eacd54
wolsos-v <mamaev757@gmail.com>
2022-07-15 12:48:08 +0300

    feat: setup swagger

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,8 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule } from '@nestjs/swagger';
+import { readFile } from 'fs/promises';
+import { resolve } from 'path';
+import { parse } from 'yaml';
 import { AppModule } from './app.module';
 import 'dotenv/config';
 
@@ -6,6 +10,13 @@ const PORT = parseInt(process.env.PORT) || 4000;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const docApiPath = resolve(process.cwd(), 'doc', 'api.yaml');
+  const docApi = await readFile(docApiPath, 'utf-8');
+  const document = parse(docApi);
+
+  SwaggerModule.setup('doc', app, document);
+
   await app.listen(PORT);
 }
 bootstrap();
